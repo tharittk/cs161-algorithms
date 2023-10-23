77e4f516831140a17d13205b71586fc521da5a51
Tharit Tangkijwanichakul <tharitt@pttep.com>
2023-10-23 11:38:42 +0700

    quick sort passed

--- QuickSort.py
+++ QuickSort.py
@@ -16,13 +16,13 @@ def QuickSort (array, l = 0, r = None):
 
     # first run case
     if r == None:
-        r = len(array)
+        r = len(array) - 1
 
     # size one array
     if (l >= r):
         return array
         
-    p = ChoosePivot(array,l ,r)
+    p = ChoosePivot(array,l ,r, 'first')
     
     # swap pivot to the first position
     array = swap(array, p, l)
@@ -38,16 +38,16 @@ def QuickSort (array, l = 0, r = None):
 # Partitioning: pivot has to be at 0
 def Partition(array, l = 0, r = -1):
 
+
     # partition boundary i i.e. array[i] is ready to be swapped
     i = l + 1
-    for j in range(l + 1, r):
+    for j in range(l + 1, r + 1):
         if array[j] < array[l]:
             array = swap(array, i, j)
             i += 1
 
     # swap pivot with latest element to lhs
     array = swap(array, i - 1, l)
-
     return array, i-1
 
 
@@ -73,19 +73,18 @@ def swap(array, index1, index2):
 
 if __name__ == "__main__":
 
-    test = [3, 0, -1 ,8, 2, 5, 1, 4, 7, 6]
+    #test = [3, 0, -1 ,8, 2, 5, 1, 4, 7, 6]
     #test = [3, 0, -1 ,8, 2, 5, 1, 4]
     #test = [0,-1]
 
-    print("test,", test)
-    result = QuickSort(test)
-    print(result)
-
-    print('is sorted:', all(result[i] <= result[i+1] for i in range(len(result) - 1)))
+    #print("test,", test)
+    #result = QuickSort(test)
+    #print(result)
+    #print('is sorted:', all(result[i] <= result[i+1] for i in range(len(result) - 1)))
 
     with open("QuickSort.txt") as f:
         arrayInput = [int(line.rstrip('\n')) for line in f]
         result = QuickSort(arrayInput)
 
-    #print('is sorted:', all(result[i] <= result[i+1] for i in range(len(result) - 1)))
+    print('is sorted:', all(result[i] <= result[i+1] for i in range(len(result) - 1)))
     #print(result)
