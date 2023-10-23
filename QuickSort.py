7fbbf5a5333c1ccf230012db03646f0e43d62fdd
Tharit Tangkijwanichakul <tharitt@pttep.com>
2023-10-23 13:22:54 +0700

    quicksort finished

--- QuickSort.py
+++ QuickSort.py
@@ -1,16 +1,4 @@
 # Quick Sort
-'''
-    # edge case - size two array
-    if (l+1) == r:
-        # swap
-        
-
-        return array, r
-    
-        # not swap
-
-        return array, l
-'''
 
 def QuickSort (array, l = 0, r = None):
 
@@ -20,20 +8,26 @@ def QuickSort (array, l = 0, r = None):
 
     # size one array
     if (l >= r):
-        return array
+        return array, 0
         
-    p = ChoosePivot(array,l ,r, 'first')
+    p = ChoosePivot(array,l ,r, 'median')
     
     # swap pivot to the first position
     array = swap(array, p, l)
 
-    array, pivotIndex = Partition(array, l, r)
+    array, pivot_index = Partition(array, l, r)
+
+    total_count = r - l
+
     # lhs part
-    array = QuickSort(array, l, pivotIndex - 1)
+    array, comp_l = QuickSort(array, l, pivot_index - 1)
     # rhs part
-    array = QuickSort(array, pivotIndex + 1, r)
-    
-    return array
+    array, comp_r = QuickSort(array, pivot_index + 1, r)
+
+    total_count += comp_l
+    total_count += comp_r
+
+    return array, total_count
 
 # Partitioning: pivot has to be at 0
 def Partition(array, l = 0, r = -1):
@@ -58,11 +52,23 @@ def ChoosePivot(array, l, r, method = 'first'):
     if method == 'last':
         return r
     if method == 'median':
-        n = len(array)
+        n = r - l + 1
         if n % 2 == 0:
-            return n //2 - 1
+            mid_index = l + n//2 - 1
         else:
-            return n//2
+            mid_index = l + n//2
+        first = array[l]
+        last = array[r]
+        mid = array[mid_index]
+        median = first + mid + last - max(first, mid, last) - min(first, mid, last)
+        if median == mid:
+            return mid_index
+        elif median == first:
+            return l
+        elif median == last:
+            return r
+        else:
+            raise ValueError
             
 def swap(array, index1, index2):
     tmp = array[index1]
@@ -72,6 +78,7 @@ def swap(array, index1, index2):
 
 
 if __name__ == "__main__":
+
 
     #test = [3, 0, -1 ,8, 2, 5, 1, 4, 7, 6]
     #test = [3, 0, -1 ,8, 2, 5, 1, 4]
@@ -84,7 +91,8 @@ if __name__ == "__main__":
 
     with open("QuickSort.txt") as f:
         arrayInput = [int(line.rstrip('\n')) for line in f]
-        result = QuickSort(arrayInput)
+        result, count = QuickSort(arrayInput)
 
     print('is sorted:', all(result[i] <= result[i+1] for i in range(len(result) - 1)))
+    print('total count:', count)
     #print(result)
