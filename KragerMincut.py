05838e24f10c15e6f15e9a0aabc51afba8af66ee
Tharit Tangkijwanichakul <TharitT@pttep.com>
2023-10-27 13:48:22 +0700

    pass

--- KragerMincut.py
+++ KragerMincut.py
@@ -10,18 +10,14 @@ class Graph():
 
     def mergeVertex(self, srcName, destName):
 
-        #print('in src: ', srcName, 'in dest: ', destName)
-
         srcNameFinal = srcName
         destNameFinal = destName
 
         if G.vtxStatus[src] == False:
-            #isChildSrc = True
             while G.vtxStatus[srcNameFinal] == False:
                 srcNameFinal = G.vtxDict[srcNameFinal].pointsToVertexName
 
         if G.vtxStatus[dest] == False:
-            #isChildDest = True
             while G.vtxStatus[destNameFinal] == False:
                 destNameFinal = G.vtxDict[destNameFinal].pointsToVertexName
 
@@ -32,33 +28,27 @@ class Graph():
         destVtx = self.vtxDict[destName]
         destVtx.remainingEdges.remove(srcName)
 
-
-
+        # to prevent case of edge within the merge group
         if srcNameFinal != destNameFinal:
 
             # set inactive
             self.vtxStatus[destNameFinal] = False 
-            #print(destNameFinal, 'becomes inactive')
-            #print('in srcFinal: ', srcNameFinal, 'in destFinal: ', destNameFinal)
 
             # consolidate edge to the parent
             srcVtxFinal = self.vtxDict[srcNameFinal]
             destVtxFinal = self.vtxDict[destNameFinal]
 
-
             # remove original name
-
             srcVtxFinal.edges.remove(destName)
             destVtxFinal.edges.remove(srcName)
 
-
+            # consolidate edge
             srcVtxFinal.edges += destVtxFinal.edges
             destVtxFinal.pointsToVertexName = srcVtxFinal.name
             destVtxFinal.edges = []
 
             # consolidate the child to the parent
             srcVtxFinal.childVertexName += (destVtxFinal.childVertexName + [destVtxFinal.name])
-
 
             # clear self-loop
             for childName in srcVtxFinal.childVertexName:
@@ -79,7 +69,6 @@ class Vertex():
         self.remainingEdges = edges[:] # to keep track
 
 
-
 def radomlySelectEdge(G):
     idxVtx = random.randint(0, len(G.vtxDict) - 1)
     randVtxName = list(G.vtxDict.keys())[idxVtx]
@@ -88,13 +77,6 @@ def radomlySelectEdge(G):
     while G.vtxDict[randVtxName].remainingEdges == []:
         idxVtx = random.randint(0, len(G.vtxDict) - 1)
         randVtxName = list(G.vtxDict.keys())[idxVtx]
-
-    # inactive vertex
-    #randVtxNameOrig= randVtxName # save original
-
-    #while G.vtxStatus[randVtxName] == False:
-    #    randVtxName = G.vtxDict[randVtxName].pointsToVertexName
-    #    isChildSrc = True
 
     idxEdge = random.randint(0, len(G.vtxDict[randVtxName].remainingEdges) -1 )
     randEdgeName = G.vtxDict[randVtxName].remainingEdges[idxEdge]
@@ -107,34 +89,37 @@ def printVtxEdgeList(G):
         print(key, G.vtxDict[key].edges, 'child: ',  G.vtxDict[key].childVertexName)
 
 if __name__ == "__main__":
-    random.seed(11)
+    results = []
+    for i in range(1):
+        random.seed(i)
 
-    G = Graph()
-    #with open('pa4.txt') as f:
-    with open('kargerMincut.txt') as f:
-    #with open('graph.txt') as f:
+        G = Graph()
+        #with open('pa4.txt') as f:
+        with open('kargerMincut.txt') as f:
+            lines = f.readlines()
+            for line in lines:
+                ve = line.strip().split('\t')
+                vtxName = ve[0]
+                edges = ve[1:]
+                
+                vertex = Vertex(vtxName, edges)
+                G.vtxDict[vertex.name] = vertex
+                G.vtxStatus[vertex.name] = True
 
-        lines = f.readlines()
-        for line in lines:
-            ve = line.strip().split('\t')
-            vtxName = ve[0]
-            edges = ve[1:]
-            
-            vertex = Vertex(vtxName, edges)
-            G.vtxDict[vertex.name] = vertex
-            G.vtxStatus[vertex.name] = True
+        while ( G.total_alive_vertices() > 2 ):
 
-    while ( G.total_alive_vertices() > 2 ):
+            src, dest = radomlySelectEdge(G)
 
-        src, dest = radomlySelectEdge(G)
+            G.mergeVertex(src, dest)
 
-        G.mergeVertex(src, dest)
+        for key in G.vtxDict.keys():
+            if G.vtxStatus[key] == True:
+                #print(key, 'edge len: ', len(G.vtxDict[key].edges))
+                result = len(G.vtxDict[key].edges)
 
-
-
-    for key in G.vtxDict.keys():
-        if G.vtxStatus[key] == True:
-            print(key, 'edge len', len(G.vtxDict[key].edges))
+        results.append(result)
+    print(results)
+    print(min(results))
 
     #printVtxEdgeList(G)
 
