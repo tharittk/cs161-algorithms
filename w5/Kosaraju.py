8ed7b91809321cb9ee52d65d0497a67b93579e6f
Tharit Tangkijwanichakul <TharitT@pttep.com>
2023-11-15 09:42:04 +0700

    fast read for big input

--- w5/Kosaraju.py
+++ w5/Kosaraju.py
@@ -13,8 +13,9 @@ class Graph():
         self.currentLeader = None
         self.currentSource = None
         self.finishingTimeCounter = None
+        self.vtxList = []
 
-    def _create_graph(self, inputFile, reverse= False):
+    def _create_graph_old(self, inputFile, reverse= False):
         with open(inputFile) as f:
             lines = f.readlines()
             for line in lines:
@@ -26,10 +27,55 @@ class Graph():
                     head = line[0]
                     tail = line[1]
 
+                if tail not in self.vtxList:
+                    self.vtxList.append(tail)
+                if head not in self.vtxList:
+                    self.vtxList.append(head)
+
                 if tail not in self.vtxDict.keys():
                     self.vtxDict[tail] = Vertex(tail, [head])
                 else:
-                    self.vtxDict[tail].edges.append(head)   
+                    self.vtxDict[tail].edges.append(head)
+
+        # append vtx that does not have outgoing edge
+        for vtx in self.vtxList:
+            if vtx not in self.vtxDict.keys():
+                self.vtxDict[vtx] = Vertex(vtx, [])
+                 
+
+    def _create_graph(self, inputFile, reverse= False):
+
+        edges = np.loadtxt(inputFile, dtype='str')
+        if not reverse:
+            tailIdx = 0
+            headIdx = 1
+        else:
+            tailIdx = 1
+            headIdx = 0
+
+        for row in range(edges.shape[0]):
+
+            tail = edges[row, ][tailIdx]
+            head = edges[row, ][headIdx]
+
+            # TOO SLOW to implement
+            #if tail not in self.vtxList:
+            #        self.vtxList.append(tail)
+            #if head not in self.vtxList:
+            #        self.vtxList.append(head)
+
+            if tail not in self.vtxDict.keys():
+                self.vtxDict[tail] = Vertex(tail, [head])
+            else:
+                self.vtxDict[tail].edges.append(head)
+
+            if row % 100000 == 0:
+                print(row)
+
+        # append vtx that does not have outgoing edge
+        #for vtx in self.vtxList:
+        #    if vtx not in self.vtxDict.keys():
+        #        self.vtxDict[vtx] = Vertex(vtx, [])
 
     def dfs(self, currentNode):
         # mark explored
@@ -48,19 +94,21 @@ class Graph():
     
     def dfs_loop(self):
         keysList = list(self.vtxDict.keys())
-        n_vtx = len(keysList)
+        n_vtx = len(self.vtxList)
 
         self.finishingTimeCounter = 0
         self.currentSource = None
 
         for i in range(n_vtx, 0, -1):
-            #key = keysList[i - 1]
-            #print('dfs loop i:', key)
             key = str(i)
             if not self.vtxDict[key].isExplored:
                 self.currentSource = str(key)
                 self.dfs(str(key))
 
+        #for key in keysList:
+        #    if not self.vtxDict[key].isExplored:
+        #        self.currentSource = key
+        #        self.dfs(key)
 
 
 
@@ -70,15 +118,28 @@ class Graph():
 
 
 if __name__ == "__main__":
-    G = Graph()
-    G._create_graph("./scc_small.txt")
-    Grev = Graph()
-    Grev._create_graph("./scc_small.txt", reverse=True)
+    import sys, threading, numpy as np
+    sys.setrecursionlimit(800000)
+    threading.stack_size(67108864)
+    def main():
 
-    #for key in G.vtxDict.keys():
-    #    print(key, G.vtxDict[key].edges)
-    
-    #for key in Grev.vtxDict.keys():
-    #    print(key, Grev.vtxDict[key].edges)
+        G = Graph()
+        G._create_graph("./scc_small.txt")
+        Grev = Graph()
+        Grev._create_graph("./scc.txt", reverse=True)
 
-    Grev.dfs_loop()
+        #for key in G.vtxDict.keys():
+        #    print(key, G.vtxDict[key].edges)
+        
+        #for key in Grev.vtxDict.keys():
+        #    print(key, Grev.vtxDict[key].edges)
+
+        #Grev.dfs_loop()
+
+        print('>>>', len(Grev.vtxList))
+
+        #for i in range(10):
+        #    print(str(arr[i,0]), str(arr[i,1]))
+
+    thread = threading.Thread(target=main)
+    thread.start()
