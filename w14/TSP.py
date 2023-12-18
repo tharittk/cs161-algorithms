c86f19689b8626f5f06a926c33810041de21bbae
Tharit Tangkijwanichakul <TharitT@pttep.com>
2023-12-18 11:23:54 +0700

    TSP Dynamic Programming - passed test

--- w14/TSP.py
+++ w14/TSP.py
@@ -8,7 +8,7 @@ class TSP():
         self.n_vertices = None
         self.v_x_y = {}
         self.distance_matrix = None
-        self.A = None
+        self.A = {}
     def read_text_input(self, input_file):
 
         with open(input_file) as f:
@@ -51,43 +51,51 @@ class TSP():
     def generate_subsets_with_size(self, list_members, size):
         return combinations(list_members, size)
     
+    def find_min(self,S, j):
+        global_min = float('inf')
+        #print('S: ',S, 'j: ', j)
+        S_minus_j = tuple(sorted(set(S) - {j}))
+        #print('S minus j', S_minus_j)
+        for k in S:
+            if k != j:
+                current_min = self.A[S_minus_j][k] + self.distance_matrix[k][j]
+                if current_min < global_min:
+                    global_min = current_min
+        return global_min
 
-    def map_subset_to_array_index (self, subset):
 
-        index = 1
-
-        return index
 
     def run(self):
-        # initialize A matrix
-        self.A = np.zeros((2**self.n_vertices, self.n_vertices + 1))
-
-        # A[S, 1] = 0 if S = {1}
-        # and + inf o.w.
-        #self.A[:, 1] = float('inf')
-        #self.A[self.map_subset_to_array_index((1,)), 1] = 0
-        
-        # cannot, we wil handle this in base-case
-        
-        
-        #print(self.A.shape)
-        #print(self.A[0:5, 1])
-
 
         vertex_list = [i for i in range(1,self.n_vertices + 1)]
+        self.A[(1,)] = np.empty(self.n_vertices + 1)
+        self.A[(1,)][1] = 0
+
         for m in range(2, self.n_vertices + 1): # size of subset
+            print('m = ', m)
             subusets = self.generate_subsets_with_size( vertex_list, m)
-
-            for subset in subusets:
+            for subset in subusets: #all subsuet size m
                 if 1 in subset:
+                    self.A[subset] = np.zeros(self.n_vertices + 1)
+                    self.A[subset][1] = float('inf') # A[S, 1] = inf if S != {1}
 
-                    # hash function -- key = subset, key 2 =vertex index , values = cost
-                    print(subset)
-            break
+                    for j in subset: # j in S and j != 1
+                        if j != 1:
+                            self.A[subset][j] = self.find_min(subset, j) # find min
 
-            #print(type(list(subusets)[0]))
-            #break
 
+        #    break
+        #print(self.A)
+        tuple_vertex_list = tuple(sorted(vertex_list))
+        global_min = float('inf')
+        #min_index = None
+        for j in range(2, self.n_vertices + 1):
+
+            current_min = self.A[tuple_vertex_list][j] + self.distance_matrix[j][1]
+            if current_min < global_min:
+                global_min = current_min
+        
+        return global_min
 
 if __name__ == "__main__":
 
@@ -99,4 +107,6 @@ if __name__ == "__main__":
     #subsets = tsp.generate_subsets_with_size([1,2,3],1)
     #print(list(subsets))
 
-    tsp.run()
+    min_cost = tsp.run()
+
+    print(">> min cost:", min_cost)
